eee079a82d37b7c3d5445892e5ad347f30ca7934
xan321 <170107136@stu.sdu.edu.kz>
2021-12-02 23:35:39 +0600

    view binding added

--- app/src/main/java/com/example/compositiongame/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/example/compositiongame/presentation/ChooseLevelFragment.kt
@@ -6,13 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.compositiongame.R
+import com.example.compositiongame.databinding.FragmentChooseLevelBinding
+import java.lang.RuntimeException
 
 class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding : FragmentChooseLevelBinding
+    get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
